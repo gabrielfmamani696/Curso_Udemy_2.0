e5aec6cf3597f2ee07ac25d9d218091acfd65c12
Mona Lisa <gabrielfmamanicatari@gmail.com>
2025-05-01 19:48:45 -0400

    hasta el video8

--- Curso_Udemy_2.0 FE/src/app/components/invoice/invoice.component.ts
+++ Curso_Udemy_2.0 FE/src/app/components/invoice/invoice.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';import { InvoiceService } from '../../services/invoice.service';
+import { Component, OnInit } from '@angular/core'; import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../models/invoice';
+import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
+import { ClientViewComponent } from '../client-view/client-view.component';
+import { CompanyViewComponent } from '../company-view/company-view.component';
+import { ListItemsComponent } from '../list-items/list-items.component';
+import { RowItemComponent } from '../row-item/row-item.component';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [],
+  imports: [InvoiceViewComponent, ClientViewComponent, CompanyViewComponent, ListItemsComponent, RowItemComponent],
   templateUrl: './invoice.component.html',
-  styleUrl: './invoice.component.css'
 })
-export class InvoiceComponent implements OnInit{
+export class InvoiceComponent implements OnInit {
   invoice!: Invoice;
-  constructor(private service: InvoiceService){
-    
+  constructor(private service: InvoiceService) {
+
   }
 
   ngOnInit(): void {
-      this.invoice=this.service.getInvoice();
+    this.invoice = this.service.getInvoice();
   }
 }
